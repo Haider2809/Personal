1db1dd0cc8d7c2fa62fc469f95274abf0f564815
Haider2809 <124145540+Haider2809@users.noreply.github.com>
2023-02-01 11:17:15 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+/* function text(){
+  var a=1;
+  if (a==1) {
+    document.getElementById("haider").style.color="Blue";
+    // document is where it has to go to find file and other  find element by id
+    a=2;
+  } else if (a==2) {
+    document.getElementById("haider").style.color="Yellow";
+    a=1;
+  }
+}*/
+  console.log("Used for printing");
+    function digitalWork(){
+      console.log("digital function works");
+}
+var a=0;
+function maps() {
+   //controls if a is 0
+  if (a==0) {
+    document.getElementById("maps").style.display="flex"; // open
+   a=1;
+  } else if (a==1) {
+    document.getElementById("maps").style.display="none"; // open
+   a=0;
+  }
+
+}
